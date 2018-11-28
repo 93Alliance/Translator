5c018a0f9610efa3e5a28fc29679a461c8c58206
vincent <jeffmanword@gmail.com>
2018-11-28 17:43:31 +0800

    修改python版本

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 #安装依赖
 sudo apt-get install xclip -y
 sudo apt-get install libnotify-bin -y
-sudo apt-get install notify-send -y
+NOTIFY=$(which notify-send)
+if [ -z $NOTIFY ]
+then
+    sudo apt-get install notify-send -y
+fi
+
 #将执行文件拷贝到系统环境下
 sudo cp -f ./src/translate.py /usr/local/bin/translate
 sudo chmod 775 /usr/local/bin/translate
@@ -20,7 +25,7 @@ sudo chmod 777 ~/ydword
 currentSystem=$(lsb_release -a | grep 'Distributor ID' 2> /dev/null)
 currentSystem=$(echo ${currentSystem#*:})
 
-if [ $currentSystem == "Ubuntu" ]
+if [ "$currentSystem" = "Ubuntu" || "$currentSystem" = "LinuxMint" ]
 then
 	gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/ name "fanyi"
 	gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/ command "fanyi"
--- src/transword.py
+++ src/transword.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7 
+#!/usr/bin/python 
 #-*- encoding: utf-8 -*- 
 """
 # 作者: vincent
